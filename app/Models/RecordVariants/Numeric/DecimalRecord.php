7739b918491d39b70aaa95fa217c70076593cdaa
Timothy Karani <kartimothy@gmail.com>
2022-09-27 13:32:20 +0300

    Beginning work on decimal records

---
+++ app/Models/RecordVariants/Numeric/DecimalRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\RecordVariants\Numeric;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DecimalRecord extends Model
+{
+    use HasFactory;
+}
---
+++ database/factories/RecordVariants/Numeric/DecimalRecordFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\RecordVariants\Numeric;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RecordVariants\Numeric\DecimalRecord>
+ */
+class DecimalRecordFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
