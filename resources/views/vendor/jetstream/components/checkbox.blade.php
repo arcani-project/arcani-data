f669aade1e42c1be4030fd3d70bcdc6a86d22358
Timothy Karani <kartimothy@gmail.com>
2022-09-27 12:37:28 +0300

    updated form

--- resources/views/vendor/jetstream/components/checkbox.blade.php
+++ resources/views/vendor/jetstream/components/checkbox.blade.php
@@ -1,1 +1,5 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<div class="pr-2">
+  <input type="checkbox" {!! $attributes->merge([
+      'class' => 'checkbox checkbox-primary',
+  ]) !!}>
+</div>
